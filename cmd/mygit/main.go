3199fa6e4af36f644b25eb5685d0040e76b77956
Ninad Bhangui <b.ninad50@gmail.com>
2024-04-29 10:18:11 +0530

    git hash-object imeplemented

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"compress/zlib"
+	"crypto/sha1"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"io"
@@ -18,6 +20,13 @@ func main() {
 	// Uncomment this block to pass the first stage!
 	catFileCmd := flag.NewFlagSet("cat-file", flag.ExitOnError)
 	catFilePrettyPrintBool := catFileCmd.Bool("p", false, "Pretty Print")
+
+	hashObjectCmd := flag.NewFlagSet("hash-object", flag.ExitOnError)
+	hashObjectWriteMode := hashObjectCmd.Bool(
+		"w",
+		false,
+		"Write the object into the object database",
+	)
 
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "usage: mygit <command> [<args>...]\n")
@@ -49,6 +58,49 @@ func main() {
 			gitObjectName := args[0]
 			catFilePrettyPrint(gitObjectName)
 		}
+	case "hash-object":
+		hashObjectCmd.Parse(os.Args[2:])
+		args := hashObjectCmd.Args()
+		if len(args) != 1 {
+			hashObjectCmd.PrintDefaults()
+		}
+		file, err := os.Open(args[0])
+		defer file.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fatal: Could not find file: %s\n", err)
+		}
+		hasher := sha1.New()
+		fileInfo, err := file.Stat()
+		if *hashObjectWriteMode {
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fatal: Could not compute file size: %s\n", err)
+			return
+		}
+		io.WriteString(hasher, fmt.Sprintf("blob %d\000", fileInfo.Size()))
+		io.Copy(hasher, file)
+		checksum := hasher.Sum(nil)
+		hash, err := hex.EncodeToString(checksum), nil
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: Could not compute hash: %s", err)
+		}
+		if *hashObjectWriteMode {
+			dir := fmt.Sprintf(".git/objects/%s", hash[:2])
+			if err := os.MkdirAll(dir, 0755); err != nil {
+				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+			}
+			outputFile, err := os.Create(fmt.Sprintf("%s/%s", dir, hash[2:]))
+			defer outputFile.Close()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "fatal: Could not write: %s\n", err)
+			}
+			file.Seek(0, io.SeekStart)
+			zlibWriter := zlib.NewWriter(outputFile)
+			defer zlibWriter.Close()
+			io.WriteString(zlibWriter, fmt.Sprintf("blob %d\000", fileInfo.Size()))
+			io.Copy(zlibWriter, file)
+		}
+		fmt.Println(hash)
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
