afa6545095a8f5f17fc8e69f0fa1aac43ef3f155
Ninad Bhangui <b.ninad50@gmail.com>
2024-04-29 14:08:29 +0530

    moved catfile into new package

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -8,8 +8,8 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strconv"
-	"strings"
+
+	"github.com/codecrafters-io/git-starter-go/internal/catfile"
 )
 
 // Usage: your_git.sh <command> <arg1> <arg2> ...
@@ -56,7 +56,7 @@ func main() {
 				catFileCmd.PrintDefaults()
 			}
 			gitObjectName := args[0]
-			catFilePrettyPrint(gitObjectName)
+			catfile.CatFilePrettyPrint(gitObjectName)
 		}
 	case "hash-object":
 		hashObjectCmd.Parse(os.Args[2:])
@@ -106,68 +106,4 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
 	}
-}
-
-func catFilePrettyPrint(gitObjectName string) {
-	matches := GetObjectsMatchingPrefix(".git/objects", gitObjectName)
-	if len(matches) > 1 {
-		fmt.Fprintf(os.Stderr, "error: short object ID %s is ambiguous", gitObjectName)
-		return
-	}
-	if len(matches) == 0 {
-		fmt.Fprintf(os.Stderr, "fatal: Not a valid object name: %s\n", gitObjectName)
-		return
-	}
-	gitObject, err := os.Open(
-		fmt.Sprintf(".git/objects/%s", matches[0]),
-	)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "fatal: Not a valid object name: %s\n", err)
-		return
-	}
-
-	r, err := zlib.NewReader(gitObject)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "fatal: Unable to decompress the object file: %s\n", err)
-	}
-	defer r.Close()
-	buf := make([]byte, 1)
-	var header []byte
-	// Read until null byte encountered
-	for {
-		n, err := r.Read(buf)
-		if n > 0 && buf[0] == 0 {
-			break
-		}
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			fmt.Fprintf(os.Stderr, "fatal: Could not read object file: %s\n", err)
-		}
-
-		header = append(header, buf[0])
-	}
-	headerParts := strings.Split(string(header), " ")
-	// kind := headerParts[0]
-	contentSize, err := strconv.ParseInt(
-		headerParts[1],
-		10,
-		64,
-	) // TODO: Check why it's int64 and why copyN needs int64 below
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "fatal: Invalid size in header of object file: %s\n", err)
-	}
-	io.CopyN(os.Stdout, r, contentSize)
-}
-
-func GetObjectsMatchingPrefix(directoryName string, prefix string) []string {
-	entries, _ := os.ReadDir(fmt.Sprintf("%s/%s", directoryName, prefix[:2]))
-	matches := []string{}
-	for _, e := range entries {
-		if strings.HasPrefix(e.Name(), prefix[2:]) {
-			matches = append(matches, fmt.Sprintf("%s/%s", prefix[:2], e.Name()))
-		}
-	}
-	return matches
 }
