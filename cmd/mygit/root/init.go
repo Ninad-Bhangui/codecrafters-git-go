708e5857e8fa0f40368f6f9cbe82d4a6bdce8a66
Ninad Bhangui <b.ninad50@gmail.com>
2024-05-05 15:42:54 +0530

    init command added after refactor

---
+++ cmd/mygit/root/init.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initialize git repository",
+	Run: func(cmd *cobra.Command, args []string) {
+		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
+			if err := os.MkdirAll(dir, 0755); err != nil {
+				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+			}
+		}
+
+		headFileContents := []byte("ref: refs/heads/main\n")
+		if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
+			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
+		}
+
+		fmt.Println("Initialized git directory")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
